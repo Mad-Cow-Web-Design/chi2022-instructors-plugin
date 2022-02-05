30ee99fd1d12bc698d2b0dfbc9a5a87c5846990e
Jason Robie <madcow@Mad-Cow.local>
2022-02-05 11:40:44 -0700

    limit the zoom on the instructor map

--- public/js/madcow-instructors-public.js
+++ public/js/madcow-instructors-public.js
@@ -1,153 +1,155 @@
-(function( $ ) {
-	'use strict';
+(function($) {
+    'use strict';
 
-	/**
-	 * All of the code for your public-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
+    /**
+     * All of the code for your public-facing JavaScript source
+     * should reside in this file.
+     *
+     * Note: It has been assumed you will write jQuery code here, so the
+     * $ function reference has been prepared for usage within the scope
+     * of this function.
+     *
+     * This enables you to define handlers, for when the DOM is ready:
+     *
+     * $(function() {
+     *
+     * });
+     *
+     * When the window is loaded:
+     *
+     * $( window ).load(function() {
+     *
+     * });
+     *
+     * ...and/or other possibilities.
+     *
+     * Ideally, it is not considered best practise to attach more than a
+     * single DOM-ready or window-load handler for a particular page.
+     * Although scripts in the WordPress core, Plugins and Themes may be
+     * practising this, we should strive to set a better example in our own work.
+     */
 
-	 /**
-	 * initMap
-	 *
-	 * Renders a Google Map onto the selected jQuery element
-	 *
-	 * @date    22/10/19
-	 * @since   5.8.6
-	 *
-	 * @param   jQuery $el The jQuery element.
-	 * @return  object The map instance.
-	 */
-	function initMap($el) {
+    /**
+     * initMap
+     *
+     * Renders a Google Map onto the selected jQuery element
+     *
+     * @date    22/10/19
+     * @since   5.8.6
+     *
+     * @param   jQuery $el The jQuery element.
+     * @return  object The map instance.
+     */
+    function initMap($el) {
 
-		// Find marker elements within map.
-		var $markers = $el.find('.marker');
+        // Find marker elements within map.
+        var $markers = $el.find('.marker');
 
-		// Create gerenic map.
-		var mapArgs = {
-			zoom: $el.data('zoom') || 16,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		};
-		var map = new google.maps.Map($el[0], mapArgs);
+        // Create gerenic map.
+        var mapArgs = {
+            minZoom: 2,
+            maxZoom: 9,
+            //zoom: $el.data('zoom') || 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map($el[0], mapArgs);
 
-		// Add markers.
-		map.markers = [];
-		$markers.each(function() {
-			initMarker($(this), map);
-		});
+        // Add markers.
+        map.markers = [];
+        $markers.each(function() {
+            initMarker($(this), map);
+        });
 
-		// Center map based on markers.
-		centerMap(map);
+        // Center map based on markers.
+        centerMap(map);
 
-		// Return map instance.
-		return map;
-	}
+        // Return map instance.
+        return map;
+    }
 
-	/**
-	 * initMarker
-	 *
-	 * Creates a marker for the given jQuery element and map.
-	 *
-	 * @date    22/10/19
-	 * @since   5.8.6
-	 *
-	 * @param   jQuery $el The jQuery element.
-	 * @param   object The map instance.
-	 * @return  object The marker instance.
-	 */
-	function initMarker($marker, map) {
+    /**
+     * initMarker
+     *
+     * Creates a marker for the given jQuery element and map.
+     *
+     * @date    22/10/19
+     * @since   5.8.6
+     *
+     * @param   jQuery $el The jQuery element.
+     * @param   object The map instance.
+     * @return  object The marker instance.
+     */
+    function initMarker($marker, map) {
 
-		// Get position from marker.
-		var lat = $marker.data('lat');
-		var lng = $marker.data('lng');
-		var latLng = {
-			lat: parseFloat(lat),
-			lng: parseFloat(lng)
-		};
+        // Get position from marker.
+        var lat = $marker.data('lat');
+        var lng = $marker.data('lng');
+        var latLng = {
+            lat: parseFloat(lat),
+            lng: parseFloat(lng)
+        };
 
-		// Create marker instance.
-		var marker = new google.maps.Marker({
-			position: latLng,
-			map: map,
-			icon: $marker.attr('data-marker')
-		});
-		// Append to reference for later use.
-		map.markers.push(marker);
+        // Create marker instance.
+        var marker = new google.maps.Marker({
+            position: latLng,
+            map: map,
+            icon: $marker.attr('data-marker')
+        });
+        // Append to reference for later use.
+        map.markers.push(marker);
 
-		// If marker contains HTML, add it to an infoWindow.
-		if ($marker.html()) {
+        // If marker contains HTML, add it to an infoWindow.
+        if ($marker.html()) {
 
-			// Create info window.
-			var infowindow = new google.maps.InfoWindow({
-				content: $marker.html()
-			});
+            // Create info window.
+            var infowindow = new google.maps.InfoWindow({
+                content: $marker.html()
+            });
 
-			// Show info window when marker is clicked.
-			google.maps.event.addListener(marker, 'click', function() {
-				infowindow.open(map, marker);
-			});
-		}
-	}
+            // Show info window when marker is clicked.
+            google.maps.event.addListener(marker, 'click', function() {
+                infowindow.open(map, marker);
+            });
+        }
+    }
 
-	/**
-	 * centerMap
-	 *
-	 * Centers the map showing all markers in view.
-	 *
-	 * @date    22/10/19
-	 * @since   5.8.6
-	 *
-	 * @param   object The map instance.
-	 * @return  void
-	 */
-	function centerMap(map) {
+    /**
+     * centerMap
+     *
+     * Centers the map showing all markers in view.
+     *
+     * @date    22/10/19
+     * @since   5.8.6
+     *
+     * @param   object The map instance.
+     * @return  void
+     */
+    function centerMap(map) {
 
-		// Create map boundaries from all map markers.
-		var bounds = new google.maps.LatLngBounds();
-		map.markers.forEach(function(marker) {
-			bounds.extend({
-				lat: marker.position.lat(),
-				lng: marker.position.lng()
-			});
-		});
+        // Create map boundaries from all map markers.
+        var bounds = new google.maps.LatLngBounds();
+        map.markers.forEach(function(marker) {
+            bounds.extend({
+                lat: marker.position.lat(),
+                lng: marker.position.lng()
+            });
+        });
 
-		// Case: Single marker.
-		if (map.markers.length == 1) {
-			map.setCenter(bounds.getCenter());
+        // Case: Single marker.
+        if (map.markers.length == 1) {
+            map.setCenter(bounds.getCenter());
 
-			// Case: Multiple markers.
-		} else {
-			map.fitBounds(bounds);
-		}
-	}
+            // Case: Multiple markers.
+        } else {
+            map.fitBounds(bounds);
+        }
+    }
 
-	// Render maps on page load.
-	$(document).ready(function() {
-		$('.acf-map').each(function() {
-			var map = initMap($(this));
-		});
-	});
+    // Render maps on page load.
+    $(document).ready(function() {
+        $('.acf-map').each(function() {
+            var map = initMap($(this));
+        });
+    });
 
-})( jQuery );
+})(jQuery);
