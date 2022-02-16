0be3156df4aa1498a65b15b935636424b576ac36
Daniel H Kemper II <danthemank@gmail.com>
2022-02-16 18:17:03 -0500

    Update madcow-instructors-public.js

--- public/js/madcow-instructors-public.js
+++ public/js/madcow-instructors-public.js
@@ -30,45 +30,53 @@
      */
 
     /**
-     * initMap
-     *
-     * Renders a Google Map onto the selected jQuery element
-     *
-     * @date    22/10/19
-     * @since   5.8.6
-     *
-     * @param   jQuery $el The jQuery element.
-     * @return  object The map instance.
-     */
-    function initMap($el) {
+	 * initMap
+	 *
+	 * Renders a Google Map onto the selected jQuery element
+	 *
+	 * @date    22/10/19
+	 * @since   5.8.6
+	 *
+	 * @param   jQuery $el The jQuery element.
+	 * @return  object The map instance.
+	 */
+	function initMap( $el ) {
 
-        // Find marker elements within map.
-        var $markers = $el.find('.marker');
+		// Find marker elements within map.
+		var $markers = $el.find('.marker');
 
-        // Create gerenic map.
-        var mapArgs = {
-            minZoom: 2,
-            maxZoom: 10,
-            //zoom: $el.data('zoom') || 16,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map($el[0], mapArgs);
+		// Create gerenic map.
+		var mapArgs = {
+			zoom        : $el.data('zoom') || 16,
+			mapTypeId   : google.maps.MapTypeId.ROADMAP
+		};
+		var map = new google.maps.Map( $el[0], mapArgs );
 
-        // Add markers.
-        map.markers = [];
-        $markers.each(function() {
-            initMarker($(this), map);
-        });
+		// Add markers.
+		map.markers = [];
+		$markers.each(function(){
+			initMarker( $(this), map );
+		});
 
-        // Center map based on markers.
-        centerMap(map);
+		// Center map based on markers.
+		centerMap( map );
 
-        // Return map instance.
-        return map;
-    }
-	
-	var infoWindows = [];
+		// Return map instance.
+		return map;
+	}
 
+	/**
+	 * initMarker
+	 *
+	 * Creates a marker for the given jQuery element and map.
+	 *
+	 * @date    22/10/19
+	 * @since   5.8.6
+	 *
+	 * @param   jQuery $el The jQuery element.
+	 * @param   object The map instance.
+	 * @return  object The marker instance.
+	 */
 	function initMarker( $marker, map ) {
 
 		// Get position from marker.
@@ -82,8 +90,7 @@
 		// Create marker instance.
 		var marker = new google.maps.Marker({
 			position : latLng,
-			map: map,
-            icon: $marker.attr('data-marker')
+			map: map
 		});
 
 		// Append to reference for later use.
@@ -93,66 +100,54 @@
 		if( $marker.html() ){
 
 			// Create info window.
-			var infoWindow = new google.maps.InfoWindow({
+			var infowindow = new google.maps.InfoWindow({
 				content: $marker.html()
 			});
 
-			infoWindows.push(infoWindow);
-
 			// Show info window when marker is clicked.
 			google.maps.event.addListener(marker, 'click', function() {
-
-				//close all
-				for (var i = 0; i < infoWindows.length; i++) {
-					infoWindows[i].close();
-				}
-				infoWindow.open( map, marker );
-
-			});
-
-			google.maps.event.addListener(map, 'click', function() {
-				infoWindow.close();
+				infowindow.open( map, marker );
 			});
 		}
 	}
 
-    /**
-     * centerMap
-     *
-     * Centers the map showing all markers in view.
-     *
-     * @date    22/10/19
-     * @since   5.8.6
-     *
-     * @param   object The map instance.
-     * @return  void
-     */
-    function centerMap(map) {
+	/**
+	 * centerMap
+	 *
+	 * Centers the map showing all markers in view.
+	 *
+	 * @date    22/10/19
+	 * @since   5.8.6
+	 *
+	 * @param   object The map instance.
+	 * @return  void
+	 */
+	function centerMap( map ) {
 
-        // Create map boundaries from all map markers.
-        var bounds = new google.maps.LatLngBounds();
-        map.markers.forEach(function(marker) {
-            bounds.extend({
-                lat: marker.position.lat(),
-                lng: marker.position.lng()
-            });
-        });
+		// Create map boundaries from all map markers.
+		var bounds = new google.maps.LatLngBounds();
+		map.markers.forEach(function( marker ){
+			bounds.extend({
+				lat: marker.position.lat(),
+				lng: marker.position.lng()
+			});
+		});
 
-        // Case: Single marker.
-        if (map.markers.length == 1) {
-            map.setCenter(bounds.getCenter());
+		// Case: Single marker.
+		if( map.markers.length == 1 ){
+			map.setCenter( bounds.getCenter() );
 
-            // Case: Multiple markers.
-        } else {
-            map.fitBounds(bounds);
-        }
-    }
+		// Case: Multiple markers.
+		} else{
+			map.fitBounds( bounds );
+		}
+	}
 
-    // Render maps on page load.
-    $(document).ready(function() {
-        $('.acf-map').each(function() {
-            var map = initMap($(this));
-        });
-    });
+	// Render maps on page load.
+	$(document).ready(function(){
+		$('.acf-map').each(function(){
+			var map = initMap( $(this) );
+		});
+	});
 
 })(jQuery);
